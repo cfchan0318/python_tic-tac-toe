134506462c9048fd7fe0b4b7807dbbb429f327aa
Chan Chi Fung, Marco <cf.chan0318@gmail.com>
2023-07-11 22:32:37 +0800

    display instruction, input with turn

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 def main():
     intro()
 
+    map = [["","",""],
+           ["","",""],
+           ["","",""]]
+    
+    winner = ""
+    curr_player = "O"
+    while winner == "":
+        x,y = input(f"Current Player: {curr_player}:    ").split(",")
+        x,y = int(x),int(y)
+        map[x][y] = curr_player
+        print(map)
+
+
 def intro():
     print("Tic Tac Toe")
     print("Instructions:")
@@ -12,5 +25,8 @@ def intro():
     print("1,0 | 1,1 | 1,2")
     print("----------------")
     print("2,0 | 2,1 | 2,2")
+
+def check_winner(map):
+    print("Checking Winner")
 
 main()
