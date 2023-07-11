3e3112ebeda8efe8a542f04ad73ac1ff658856a8
Chan Chi Fung, Marco <cf.chan0318@gmail.com>
2023-07-11 21:46:30 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+    intro()
+
+def intro():
+    print("Tic Tac Toe")
+    print("Instructions:")
+    print("O goes first, X follows")
+    print("enter your position with the help below:")
+
+    print("0,0 | 0,1 | 0,2")
+    print("----------------")
+    print("1,0 | 1,1 | 1,2")
+    print("----------------")
+    print("2,0 | 2,1 | 2,2")
+
+main()
